477c3d2aa56622b169c346be188ce3d24d19bf44
Roman Pijacek <pijacek.roman@gmail.com>
2019-02-16 13:49:41 +0100

    Update GetLiveQueryPlan.sql
    Added the new xEvent query_thread_profile.

--- Get_Live_Execution_Plan/GetLiveQueryPlan.sql
+++ Get_Live_Execution_Plan/GetLiveQueryPlan.sql
@@ -1,7 +1,11 @@
 USE master;
 GO
 
-IF EXISTS(SELECT 1 FROM sys.server_event_sessions WHERE name = 'GetLiveQueryPlan')  
+-------------------------------------------------------------------------------------------------
+-- Extended Event Session query_post_execution_showplan (Before SQL Server 2014 SP2/2016)
+-------------------------------------------------------------------------------------------------
+
+IF EXISTS(SELECT 1 FROM sys.server_event_sessions WHERE name = N'GetLiveQueryPlan')  
 BEGIN
     DROP EVENT SESSION GetLiveQueryPlan ON SERVER;  
 END
@@ -32,7 +36,7 @@ ALTER EVENT SESSION GetLiveQueryPlan ON SERVER STATE = STOP;
 GO
 
 -- Drop the event session
-IF EXISTS(SELECT 1 FROM sys.server_event_sessions WHERE name = 'GetLiveQueryPlan')  
+IF EXISTS(SELECT 1 FROM sys.server_event_sessions WHERE name = N'GetLiveQueryPlan')  
 BEGIN
     DROP EVENT SESSION GetLiveQueryPlan ON SERVER;  
 END
@@ -45,5 +49,60 @@ SELECT
 FROM 
     sys.dm_xe_objects 
 WHERE 
-    name = 'query_post_execution_showplan';
+    name = N'query_post_execution_showplan';
 GO
+
+-------------------------------------------------------------------------------------------------
+-- Extended Event Session query_thread_profile (the new lightweight profiling infrastructure)
+-------------------------------------------------------------------------------------------------
+
+IF EXISTS(SELECT 1 FROM sys.server_event_sessions WHERE name = N'GetLiveQueryPlan')  
+BEGIN
+    DROP EVENT SESSION GetLiveQueryPlan ON SERVER;  
+END
+GO
+ 
+CREATE EVENT SESSION GetLiveQueryPlan 
+ON SERVER
+ADD EVENT sqlserver.query_thread_profile
+(
+    ACTION (sqlserver.database_name) WHERE (database_name = N'AdventureWorks2017')
+)
+ADD TARGET package0.event_file
+(
+    SET filename = N'GetLiveQueryPlan', max_file_size = (5)
+)
+WITH
+(
+    STARTUP_STATE = OFF
+);
+GO
+
+-- Start the event session  
+ALTER EVENT SESSION GetLiveQueryPlan ON SERVER STATE = START;  
+GO  
+
+-- Stop the event session  
+ALTER EVENT SESSION GetLiveQueryPlan ON SERVER STATE = STOP;  
+GO
+
+-- Drop the event session
+IF EXISTS(SELECT 1 FROM sys.server_event_sessions WHERE name = N'GetLiveQueryPlan')  
+BEGIN
+    DROP EVENT SESSION GetLiveQueryPlan ON SERVER;  
+END
+GO
+
+-- Event's description
+SELECT 
+    name, 
+    description 
+FROM 
+    sys.dm_xe_objects 
+WHERE 
+    name = N'query_thread_profile';
+GO
+
+-------------------------------------------------------------------------------------------------
+-- EOF
+-------------------------------------------------------------------------------------------------
